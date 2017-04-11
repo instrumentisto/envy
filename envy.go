b1cfa2b0e32fde063fdca20f181f3c0223853da6
tyranron <tyranron@gmail.com>
2017-04-11 21:16:57 +0300

    Impl basic types parsing & nested crawling

--- envy.go
+++ envy.go
@@ -2,10 +2,68 @@ package envy
 
 import (
 	"errors"
+	"os"
+	refl "reflect"
+	"strconv"
+)
+
+var (
+	ErrUnsupportedType = errors.New("envy: unsupprted type") // TODO: more info
 )
 
 type Parser struct{}
 
 func (p Parser) Parse(obj interface{}) error {
-	return errors.New("unimplemented")
+	return p.parseStruct(refl.ValueOf(obj).Elem())
+}
+
+func (p Parser) parseStruct(structVal refl.Value) error {
+	structType := structVal.Type()
+	for i := 0; i < structVal.NumField(); i++ {
+		fieldVal := structVal.Field(i)
+		if !fieldVal.CanSet() {
+			continue
+		}
+		fieldKind := fieldVal.Kind()
+		envVarName, ok := structType.Field(i).Tag.Lookup("env")
+		if !ok && (fieldKind != refl.Struct) {
+			continue
+		}
+		envValue := os.Getenv(envVarName) // TODO: do not parse always
+		switch fieldKind {
+		case refl.Bool:
+			val, err := strconv.ParseBool(envValue)
+			if err != nil {
+				return err
+			}
+			fieldVal.SetBool(val)
+		case refl.String:
+			fieldVal.SetString(envValue)
+		case refl.Int, refl.Int8, refl.Int16, refl.Int32, refl.Int64:
+			val, err := strconv.ParseInt(envValue, 0, fieldVal.Type().Bits())
+			if err != nil {
+				return err
+			}
+			fieldVal.SetInt(val)
+		case refl.Uint, refl.Uint8, refl.Uint16, refl.Uint32, refl.Uint64:
+			val, err := strconv.ParseUint(envValue, 0, fieldVal.Type().Bits())
+			if err != nil {
+				return err
+			}
+			fieldVal.SetUint(val)
+		case refl.Float32, refl.Float64:
+			val, err := strconv.ParseFloat(envValue, fieldVal.Type().Bits())
+			if err != nil {
+				return err
+			}
+			fieldVal.SetFloat(val)
+		case refl.Struct:
+			if err := p.parseStruct(fieldVal); err != nil {
+				return err
+			}
+		default:
+			return ErrUnsupportedType
+		}
+	}
+	return nil
 }
--- envy_test.go
+++ envy_test.go
@@ -14,183 +14,163 @@ func TestParse(t *testing.T) {
 		Convey("bool", func() {
 			setEnv("BOOL", "false")
 			obj := &struct {
-				v bool `env:"BOOL"`
+				V bool `env:"BOOL"`
 			}{true}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldBeFalse)
+			So(obj.V, ShouldBeFalse)
 		})
 
 		Convey("string", func() {
 			setEnv("STRING", "foo")
 			obj := &struct {
-				v string `env:"STRING"`
+				V string `env:"STRING"`
 			}{"bar"}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual, "foo")
+			So(obj.V, ShouldEqual, "foo")
 		})
 
 		Convey("int", func() {
 			setEnv("INT", "0")
 			obj := &struct {
-				v int `env:"INT"`
+				V int `env:"INT"`
 			}{-1}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual, int(0))
+			So(obj.V, ShouldEqual, int(0))
 		})
 
 		Convey("int8", func() {
 			setEnv("INT8", "-123")
 			obj := &struct {
-				v int8 `env:"INT8"`
+				V int8 `env:"INT8"`
 			}{}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual, int8(-127))
+			So(obj.V, ShouldEqual, int8(-123))
 		})
 
 		Convey("int16", func() {
 			setEnv("INT16", "-32760")
 			obj := &struct {
-				v int16 `env:"INT16"`
+				V int16 `env:"INT16"`
 			}{}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual, int16(-32760))
+			So(obj.V, ShouldEqual, int16(-32760))
 		})
 
 		Convey("int32", func() {
 			setEnv("INT32", "-8388600")
 			obj := &struct {
-				v int32 `env:"INT32"`
+				V int32 `env:"INT32"`
 			}{}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual, int32(-8388600))
+			So(obj.V, ShouldEqual, int32(-8388600))
 		})
 
 		Convey("int64", func() {
 			setEnv("INT64", "-2147483640")
 			obj := &struct {
-				v int64 `env:"INT64"`
+				V int64 `env:"INT64"`
 			}{}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual, int64(-2147483640))
+			So(obj.V, ShouldEqual, int64(-2147483640))
 		})
 
 		Convey("uint", func() {
 			setEnv("UINT", "0")
 			obj := &struct {
-				v uint `env:"UINT"`
+				V uint `env:"UINT"`
 			}{1}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual, uint(0))
+			So(obj.V, ShouldEqual, uint(0))
 		})
 
 		Convey("uint8", func() {
 			setEnv("UINT8", "250")
 			obj := &struct {
-				v uint8 `env:"UINT8"`
+				V uint8 `env:"UINT8"`
 			}{}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual, uint8(250))
+			So(obj.V, ShouldEqual, uint8(250))
 		})
 
 		Convey("uint16", func() {
 			setEnv("UINT16", "65530")
 			obj := &struct {
-				v uint16 `env:"UINT16"`
+				V uint16 `env:"UINT16"`
 			}{}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual, uint16(65530))
+			So(obj.V, ShouldEqual, uint16(65530))
 		})
 
 		Convey("uint32", func() {
 			setEnv("UINT32", "16777210")
 			obj := &struct {
-				v uint32 `env:"UINT32"`
+				V uint32 `env:"UINT32"`
 			}{}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual, uint32(16777210))
+			So(obj.V, ShouldEqual, uint32(16777210))
 		})
 
 		Convey("uint64", func() {
 			setEnv("UINT64", "4294967290")
 			obj := &struct {
-				v uint64 `env:"UINT64"`
+				V uint64 `env:"UINT64"`
 			}{}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual, uint64(4294967290))
+			So(obj.V, ShouldEqual, uint64(4294967290))
 		})
 
 		Convey("BYTE", func() {
 			setEnv("BYTE", "255")
 			obj := &struct {
-				v byte `env:"BYTE"`
+				V byte `env:"BYTE"`
 			}{}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual, byte(255))
+			So(obj.V, ShouldEqual, byte(255))
 		})
 
 		Convey("rune", func() {
 			setEnv("RUNE", "8388600")
 			obj := &struct {
-				v rune `env:"RUNE"`
+				V rune `env:"RUNE"`
 			}{}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual, rune(8388600))
+			So(obj.V, ShouldEqual, rune(8388600))
 		})
 
 		Convey("float32", func() {
 			setEnv("FLOAT32", "3.40282346638528859811704183484516925440e+38")
 			obj := &struct {
-				v float32 `env:"FLOAT32"`
+				V float32 `env:"FLOAT32"`
 			}{}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual,
+			So(obj.V, ShouldEqual,
 				float32(3.40282346638528859811704183484516925440e+38))
 		})
 
 		Convey("float64", func() {
 			setEnv("FLOAT64", "1.797693134862315708145274237317043567981e+308")
 			obj := &struct {
-				v float64 `env:"FLOAT64"`
+				V float64 `env:"FLOAT64"`
 			}{}
 			err := p.Parse(obj)
 			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual,
+			So(obj.V, ShouldEqual,
 				float64(1.797693134862315708145274237317043567981e+308))
-		})
-
-		Convey("complex64", func() {
-			setEnv("COMPLEX64", "2+3i")
-			obj := &struct {
-				v complex64 `env:"COMPLEX64"`
-			}{}
-			err := p.Parse(obj)
-			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual, complex64(2+3i))
-		})
-
-		Convey("complex128", func() {
-			setEnv("COMPLEX128", "-4352342-777774i")
-			obj := &struct {
-				v complex128 `env:"COMPLEX128"`
-			}{}
-			err := p.Parse(obj)
-			So(err, ShouldBeNil)
-			So(obj.v, ShouldEqual, complex128(-4352342-777774i))
 		})
 
 	})
@@ -198,7 +178,7 @@ func TestParse(t *testing.T) {
 	Convey("On unsupported type", t, func() {
 		setEnv("UNSUPPORTED_TYPE", "2")
 		obj := &struct {
-			v uintptr `env:"UNSUPPORTED_TYPE"`
+			V uintptr `env:"UNSUPPORTED_TYPE"`
 		}{5}
 		err := Parser{}.Parse(obj)
 
@@ -208,7 +188,7 @@ func TestParse(t *testing.T) {
 		})
 
 		Convey("Does not mutate value", func() {
-			So(obj.v, ShouldEqual, uintptr(5))
+			So(obj.V, ShouldEqual, uintptr(5))
 		})
 
 	})
@@ -218,24 +198,24 @@ func TestParse(t *testing.T) {
 		setEnv("NESTED_INT", "-10")
 		setEnv("NESTED_UINT", "15")
 		obj := &struct {
-			v bool `env:"NESTED_BOOL"`
-			n struct {
-				v int `env:"NESTED_INT"`
-				n struct {
-					n struct {
-						v uint `env:"NESTED_UINT"`
+			V bool `env:"NESTED_BOOL"`
+			N struct {
+				V int `env:"NESTED_INT"`
+				N struct {
+					N struct {
+						V uint `env:"NESTED_UINT"`
 					}
 				}
 			}
 		}{}
 		err := Parser{}.Parse(obj)
 		So(err, ShouldBeNil)
-		So(obj.v, ShouldEqual, true)
-		So(obj.n.v, ShouldEqual, int(-10))
-		So(obj.n.n.n.v, ShouldEqual, uint(15))
+		So(obj.V, ShouldEqual, true)
+		So(obj.N.V, ShouldEqual, int(-10))
+		So(obj.N.N.N.V, ShouldEqual, uint(15))
 	})
 
-	Convey("Parses values for types behind pointers", t, func() {
+	/*Convey("Parses values for types behind pointers", t, func() {
 		setEnv("DEREF_BOOL", "true")
 		setEnv("DEREF_INT", "-10")
 		v := 5; ptr1 := &v; ptr2 := &ptr1
@@ -251,7 +231,7 @@ func TestParse(t *testing.T) {
 		So(err, ShouldBeNil)
 		So(*obj.v, ShouldEqual, true)
 		So(***(*obj.n).v, ShouldEqual, int(-10))
-	})
+	})*/
 }
 
 func setEnv(name, val string) {
@@ -261,7 +241,7 @@ func setEnv(name, val string) {
 }
 
 func unsetEnv(name string) {
-	if err := os.Unsetenv("CONF_DEBUG_MODE"); err != nil {
+	if err := os.Unsetenv(name); err != nil {
 		panic(err.Error())
 	}
 }
